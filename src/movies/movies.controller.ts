3fd0ceb2a1de7a52e6dd8c85497f29932fde127f
jungho shin <wjdghtls11@gmail.com>
2022-05-13 17:55:58 +0900

    Add: CRUD API 구현중

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'this will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `we are searching for a movie made after ${searchingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return `this will return a one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return `this will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
