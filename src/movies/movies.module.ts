f58349fdbeb62de1994b7bef4188651d801b47fa
jungho shin <wjdghtls11@gmail.com>
2022-05-30 19:45:59 +0900

    Add: repository 생성 및 연동

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,11 @@
+import { MovieRepository } from './movies.repository';
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MovieRepository])],
   controllers: [MoviesController],
   providers: [MoviesService],
 })
